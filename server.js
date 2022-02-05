80d47c043f5a30ae00558d1fcb91b650b408b53c
Luis Felipe <luisfelrib@gmail.com>
2022-02-05 01:44:55 -0300

    :wrench: set .env_prod as default

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-require('dotenv').config({ path: `.env_${process.env.NODE_ENV}.env` });
+const path = process.env.NODE_ENV ? `.env_${process.env.NODE_ENV}.env` : '.env_prod.env';
+require('dotenv').config({ path });
 // require('./src/infrastructure/database');
 const { startServer } = require('./src/infrastructure/express_server');
 
