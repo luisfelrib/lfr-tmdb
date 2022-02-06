fbb8c80e2e13b23b99afd22433ef5381449763ee
Luis Felipe <luisfelrib@gmail.com>
2022-02-06 00:41:31 -0300

    :wrench: added env var to enable/disable cache

--- src/port/cache/redis.js
+++ src/port/cache/redis.js
@@ -1,34 +1,42 @@
+/* eslint-disable no-console */
 const redis = require('redis');
 
-const redisClient = redis.createClient({
+const redisClient = process.env.CACHE_ENABLE === 'true' ? redis.createClient({
   url: `redis://:${process.env.REDIS_PASS}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
-});
-redisClient.on('error', (error) => {
-  // eslint-disable-next-line no-console
-  console.error(error);
-});
-redisClient.connect((err) => {
-  // eslint-disable-next-line no-console
-  if (err) console.error(err);
-});
+}) : null;
+
+if (redisClient) {
+  redisClient.connect((err) => {
+    if (err) console.error(err);
+  });
+  redisClient.on('error', (error) => {
+    console.error(error);
+  });
+}
 
 const setCache = async (key, value) => {
-  try {
-    const result = await redisClient.set(key, JSON.stringify(value));
-    return result;
-  } catch (error) {
-    return error;
+  if (redisClient) {
+    try {
+      const result = await redisClient.set(key, JSON.stringify(value));
+      return result;
+    } catch (error) {
+      return error;
+    }
   }
+  return null;
 };
 
 const getCache = async (key) => {
-  try {
-    const result = await redisClient.get(key);
-    if (result) return JSON.parse(result);
-    return result;
-  } catch (error) {
-    return error;
+  if (redisClient) {
+    try {
+      const result = await redisClient.get(key);
+      if (result) return JSON.parse(result);
+      return result;
+    } catch (error) {
+      return error;
+    }
   }
+  return null;
 };
 
 module.exports = {
